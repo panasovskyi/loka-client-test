cb3bf8f181841801a864888d80e263529205881f
panasovskyi <ie.panasovskyi@gmail.com>
2025-11-27 21:20:24 +0000

    s

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import withSvgr from "next-svgr";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
   reactCompiler: true,
   output: "export",
   basePath: "/loka-client-test", // н
