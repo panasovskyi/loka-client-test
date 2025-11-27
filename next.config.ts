a4d064222e3cacdeebe8c5d1fce32a842f15ad7c
panasovskyi <ie.panasovskyi@gmail.com>
2025-11-27 21:02:37 +0000

    Prepare for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  output: "export",
+  basePath: "/loka-client-test", // н
 };
 
 export default withSvgr(nextConfig);
